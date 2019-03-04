fa9536c862f5108a2fcdd0d907d51ae500171442
claire Roughan <claire_roughan@yahoo.co.uk>
2019-03-04 17:02:41 +0000

    Removed Extensions file

---
+++ SmoooothOperator/RoundStyleImageView.swift
@@ -0,0 +1,21 @@
+//
+//  RoundStyleImageView.swift
+//  SmoooothOperator
+//
+//  Created by claire.roughan on 04/03/2019.
+//  Copyright © 2019 claire.roughan. All rights reserved.
+//
+
+import UIKit
+
+class RoundStyleImageView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
