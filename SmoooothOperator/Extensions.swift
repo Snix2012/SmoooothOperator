12f5bf289fa1b9dcee6b530112ef2a90899411ad
claire Roughan <claire_roughan@yahoo.co.uk>
2019-03-04 15:27:14 +0000

    Added constants for image URLS and Labels text

---
+++ SmoooothOperator/Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  Extensions.swift
+//  SmoooothOperator
+//
+//  Created by claire.roughan on 01/03/2019.
+//  Copyright © 2019 claire.roughan. All rights reserved.
+//
+
+import Foundation
---
+++ SmoooothOperator/SadeTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  SadeTableViewCell.swift
+//  SmoooothOperator
+//
+//  Created by claire.roughan on 28/02/2019.
+//  Copyright © 2019 claire.roughan. All rights reserved.
+//
+
+import UIKit
+
+class SadeTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var cellImageView: UIImageView!
+    @IBOutlet weak var stretchyLbl: UILabel!
+
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        // Initialization code
+//    }
+//
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
+
+}
